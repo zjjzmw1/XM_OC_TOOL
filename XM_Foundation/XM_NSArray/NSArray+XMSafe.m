c79f0afab06c1faf768b7718d52304b5e8608761
zhangmingwei <zjjzmw1@163.com>
2019-08-26 11:37:08 +0800

    NSArray+XMSafe.m越界、插入nil崩溃的处理

--- XM_Foundation/XM_NSArray/NSArray+XMSafe.m
+++ XM_Foundation/XM_NSArray/NSArray+XMSafe.m
@@ -8,22 +8,31 @@
 
 #import "NSArray+XMSafe.h"
 #import <objc/runtime.h>
+#import "NSObject+XMRuntime.h"
 
 @implementation NSArray (XMSafe)
 
 /// 编译项目的时候就会执行，不用担心不执行。
 + (void)load {
-    // 选择器
+    // 数组越界的处理 ----------------------BEGIN-------------------------------
     SEL safeSel = @selector(xm_safeObjectAtIndex:);
     SEL unsafeSel = @selector(objectAtIndex:);
     Class class = NSClassFromString(@"__NSArrayI");
-    // 方法
     Method safeMethod = class_getInstanceMethod(class, safeSel);
     Method unsafeMethod = class_getInstanceMethod(class, unsafeSel);
-    // 交换方法
     method_exchangeImplementations(unsafeMethod, safeMethod);
+    // 数组越界的处理 ----------------------END-------------------------------
+    // 数组插入 nil 的处理 ----------------------BEGIN-------------------------------
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        NSMutableArray *obj = [[NSMutableArray alloc] init];
+        // 对象方法 __NSArrayM 和 __NSArrayI 都有实现，都要swizz
+        [obj xm_swizzleMethod:@selector(addObject:) swizzledSelector:@selector(xm_addObject:)];
+        [obj xm_swizzleMethod:@selector(insertObject:atIndex:) swizzledSelector:@selector(xm_insertObject:atIndex:)];
+    });
+    // 数组插入 nil 的处理 ----------------------END-------------------------------
 }
-
+/// 数组越界的处理
 - (id)xm_safeObjectAtIndex:(NSUInteger)index {
     // 数组越界也不会崩，但是开发的时候并不知道数组越界
     if (index > (self.count - 1)) { // 数组越界
@@ -31,6 +40,29 @@
         return nil;
     } else { // 没有越界
         return [self xm_safeObjectAtIndex:index];
+    }
+}
+/// 插入nil的处理
+- (void)xm_addObject:(id)obj {
+    if (obj) {
+        [self xm_addObject:obj];
+    } else {
+        // NSAssert 只有开发的时候才会造成程序崩溃，方便调试解决问题
+        NSAssert(NO, @"尝试插入 nil :xm_addObject");
+    }
+}
+/// 插入空，越界的崩溃处理
+- (void)xm_insertObject:(id)obj atIndex:(NSUInteger)index {
+    // 数组越界也不会崩，但是开发的时候并不知道数组越界
+    if (index > (self.count - 1)) { // 数组越界
+        NSAssert(NO, @"数组越界了,xm_insertObject"); // NSAssert 只有开发的时候才会造成程序崩了
+    } else { // 没越界
+        if (obj) { // 不为空
+            [self xm_insertObject:obj atIndex:index];
+        } else {
+            // NSAssert 只有开发的时候才会造成程序崩溃，方便调试解决问题
+            NSAssert(NO, @"尝试插入 nil :xm_insertObject");
+        }
     }
 }
 
