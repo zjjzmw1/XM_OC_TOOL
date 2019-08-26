da4fab0e93fc1a1135ecb75508a067ddbdf27d30
zhangmingwei <zjjzmw1@163.com>
2019-08-26 11:21:25 +0800

    添加NSObject 的runtime方法交互

---
+++ XM_Foundation/XM_NSObject/NSObject+XMRuntime.h
@@ -0,0 +1,25 @@
+//
+//  NSObject+XMRuntime.h
+//  艺库
+//
+//  Created by zhangmingwei on 2019/8/26.
+//  Copyright © 2019 YiKuNetwork. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <objc/runtime.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+/// runtime便利的使用方法
+@interface NSObject (XMRuntime)
+
+/// 获取协议的列表
++ (void)getProtocolList_XM;
+
+/// 方法交互
+- (void)xm_swizzleMethod:(SEL)originSelector swizzledSelector:(SEL)swizzleSelector;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ XM_Foundation/XM_NSObject/NSObject+XMRuntime.m
@@ -0,0 +1,41 @@
+//
+//  NSObject+XMRuntime.m
+//  艺库
+//
+//  Created by zhangmingwei on 2019/8/26.
+//  Copyright © 2019 YiKuNetwork. All rights reserved.
+//
+
+#import "NSObject+XMRuntime.h"
+
+@implementation NSObject (XMRuntime)
+
+/// 获取协议的列表
++ (void)getProtocolList_XM {
+    unsigned int count;
+    __unsafe_unretained Protocol **protocolList = class_copyProtocolList([self class], &count);
+    for (unsigned int i = 0; i < count; i++) {
+        Protocol *pro = protocolList[i];
+        const char *proName = protocol_getName(pro);
+        XMLog(@"proName = %@",[NSString stringWithUTF8String:proName]);
+    }
+}
+
+/// 方法交互
+- (void)xm_swizzleMethod:(SEL)originSelector swizzledSelector:(SEL)swizzleSelector {
+    Class class = [self class];
+    // 源方法
+    Method originM = class_getInstanceMethod(class, originSelector);
+    // 要交互的方法
+    Method swizzleM = class_getInstanceMethod(class, swizzleSelector);
+    
+    BOOL didAddMethod = class_addMethod(class, originSelector, method_getImplementation(swizzleM), method_getTypeEncoding(swizzleM));
+    // 如果 YES 证明之前的originSelector不存在，会给类添加一个方法originalSelector
+    if (didAddMethod) {
+        class_replaceMethod(class, swizzleSelector, method_getImplementation(originM), method_getTypeEncoding(originM));
+    } else { // 之前有originSelector方法
+        method_exchangeImplementations(originM, swizzleM);
+    }
+}
+
+@end
